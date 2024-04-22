a443259ae3ea9d40909cd30028fb84ed651fbc52
Felipe Cristian <fc.fullstackdeveloper@gmail.com>
2024-04-22 12:32:34 -0300

    Configurando o menu mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,13 +6,13 @@ import iconWhatsapp from '../../assets/whatsapp.png';
 
 const Header = () => {
 
-    const btnMobile = document.querySelector('#btn-mobile');
-    const menuOpen = document.querySelector('.nav-list');
+    function openMenu() {
+        const btnMobile = document.querySelector('#btn-mobile');
+        const menuOpen = document.querySelector('#nav-list');
 
-    function handleClick() {
         btnMobile.classList.toggle('active');
         menuOpen.classList.toggle('active');
-    }
+   }
 
   return (
     
@@ -23,7 +23,7 @@ const Header = () => {
             </div>
 
             <nav className="nav-menu">
-                <ul className="nav-list">
+                <ul className="nav-list" id="nav-list">
                     <li><a href="#"><span class="material-symbols-outlined">home</span>Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
@@ -37,7 +37,7 @@ const Header = () => {
                 <a href="#" target="_blank"><img src={iconWhatsapp} alt="" /></a>
             </div>
 
-            <button className="btn-mobile" id='btn-mobile' onClick={handleClick}>
+            <button className="btn-mobile" id='btn-mobile'onClick={openMenu}>
                 <span></span>
                 <span></span>
                 <span></span>
