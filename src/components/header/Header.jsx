9f0aa137a81fdc4d0eef1cabf9dfdf068cd56b55
Felipe Cristian <fc.fullstackdeveloper@gmail.com>
2024-04-22 12:55:34 -0300

    Alterado a extensão do arquivo index, dentro da dist

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,39 +12,39 @@ const Header = () => {
 
         btnMobile.classList.toggle('active');
         menuOpen.classList.toggle('active');
-   }
+    }
 
-  return (
-    
-    <div className='header'>
-        <div className="navbar">
-            <div className="nav-logo">
-                <a href="#"><h2>Porfólio</h2></a>
+    return (
+
+        <div className='header'>
+            <div className="navbar">
+                <div className="nav-logo">
+                    <a href="#"><h2>Porfólio</h2></a>
+                </div>
+
+                <nav className="nav-menu">
+                    <ul className="nav-list" id="nav-list">
+                        <li><a href="#"><span class="material-symbols-outlined">home</span>Home</a></li>
+                        <li><a href="#about"><span class="material-symbols-outlined">account_circle</span>About</a></li>
+                        <li><a href="#projects"><span class="material-symbols-outlined">folder</span>Projects</a></li>
+                        <li><a href="#contact"><span class="material-symbols-outlined">phone_in_talk</span>Contact</a></li>
+                    </ul>
+                </nav>
+
+                <div className="socials-media">
+                    <a href="#" target="_blank"><img src={iconLinkedin} alt="" /></a>
+                    <a href="#" target="_blank"><img src={iconGithub} alt="" /></a>
+                    <a href="#" target="_blank"><img src={iconWhatsapp} alt="" /></a>
+                </div>
+
+                <button className="btn-mobile" id='btn-mobile' onClick={openMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
-
-            <nav className="nav-menu">
-                <ul className="nav-list" id="nav-list">
-                    <li><a href="#"><span class="material-symbols-outlined">home</span>Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-
-            <div className="socials-media">
-                <a href="#" target="_blank"><img src={iconLinkedin} alt="" /></a>
-                <a href="#" target="_blank"><img src={iconGithub} alt="" /></a>
-                <a href="#" target="_blank"><img src={iconWhatsapp} alt="" /></a>
-            </div>
-
-            <button className="btn-mobile" id='btn-mobile'onClick={openMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header;
